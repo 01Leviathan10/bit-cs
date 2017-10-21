9e2db331ccfeab91893cf77b2f000d7fdf7c1867
01Leviathan10 <markomilicevic@Markos-MacBook-Pro.local>
2017-10-21 18:44:46 +0200

    Vukend vezba

---
+++ vezbaonica/singleString.js
@@ -0,0 +1,7 @@
+var x = ['1', 'A', 'B', "c", "r", true, NaN, undefined];
+var string='';
+for (var i = 0; i < x.length; i++) {
+    string+=x[i] 
+}
+console.log(string);
+
---
+++ vezbaonica/sum.js
@@ -0,0 +1,14 @@
+var sum1;
+var sum0;
+for (var i = 0; i < 1000; i++) {
+    if ((i%3)==0) {
+        sum = i;
+        console.log(sum);
+    }
+    else if ((i%5)==0){
+        sum1 = i;
+        console.log(sum1);
+    }
+
+}
+    
