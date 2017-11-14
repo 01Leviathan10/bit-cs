3670f606a812476fe4340c5fa7e5b6710ed6fd77
BIT Student <classroom@bgit.rs>
2017-11-14 17:32:36 +0100

    Blog to complite

---
+++ React/blog/components/header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Timer from "./timer";
+const Header = function(){
+    
+    return (
+        <header>
+            <h1 class ="h1">Blog</h1>
+            <span class="span">Homeeee</span>
+          
+            <span class="span">Authorsss</span>
+            <span class="span">Abouttt</span>
+            {/* <Timer interval="2000"/> */}
+        </header>
+    );
+};
+
+
+
+export default Header;
